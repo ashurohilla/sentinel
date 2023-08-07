7c563e83f11082201911a3c4c190434358932f01
Ashish <ashishrohilla510@gmail.com>
2023-08-08 00:33:07 +0530

    adding desktopfiles

--- workbench/api/dashboard/models.py
+++ workbench/api/dashboard/models.py
@@ -164,8 +164,10 @@ class screenshotsModel(models.Model):
 
 
 class desktopfile(models.Model):
+    name = models.CharField(null=True, max_length=50)
     file = models.FileField(upload_to='desktopapp/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
     
     
  
